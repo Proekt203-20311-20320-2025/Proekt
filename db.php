8ae22de22fa4bfba3efe46d2370821e1d39d87dd
DenisP803 <denispapanchev3@abv.bg>
2025-03-19 22:42:46 +0200

    Стилизация и бази данни
    Стилизиране на сайта и връзката с бази данни

--- db.php
+++ db.php
@@ -1,1 +1,12 @@
+<?php
+$host = 'localhost';
+$dbName = 'air_cond_db'; // името на вашата база
+$user = 'root';          // потребител
+$pass = '';              // парола (ако има)
 
+$connection = mysqli_connect($host, $user, $pass, $dbName);
+
+if (!$connection) {
+    die("Грешка при свързване с базата: " . mysqli_connect_error());
+}
+?>
